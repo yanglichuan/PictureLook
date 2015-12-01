4070b5c736569622db2b6733212822c2ae156956
yanglichuan <yanglichuancom@163.com>
2015-12-01 18:03:14 +0800

    PictureLook

---
+++ VIPhotoViewDemo/ViewController.m
@@ -0,0 +1,42 @@
+//
+//  ViewController.m
+//  VIPhotoViewDemo
+//
+//  Created by Vito on 1/7/15.
+//  Copyright (c) 2015 vito. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "VIPhotoView.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    UIImage *image = [UIImage imageNamed:@"test.jpg"];
+    VIPhotoView *photoView = [[VIPhotoView alloc] initWithFrame:self.view.bounds andImage:image];
+    photoView.autoresizingMask = (1 << 6) -1;
+    
+    [self.view addSubview:photoView];
+}
+
+- (void)viewDidLayoutSubviews
+{
+    [super viewDidLayoutSubviews];
+    NSLog(@"%@", NSStringFromCGRect([[[self.view subviews] lastObject] frame]));
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
+// 版权属于原作者
+// http://code4app.com (cn) http://code4app.net (en)
+// 发布代码于最专业的源码分享网站: Code4App.com
---
+++ VIPhotoViewDemoTests/VIPhotoViewDemoTests.m
@@ -0,0 +1,43 @@
+//
+//  VIPhotoViewDemoTests.m
+//  VIPhotoViewDemoTests
+//
+//  Created by Vito on 1/7/15.
+//  Copyright (c) 2015 vito. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface VIPhotoViewDemoTests : XCTestCase
+
+@end
+
+@implementation VIPhotoViewDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
+// 版权属于原作者
+// http://code4app.com (cn) http://code4app.net (en)
+// 发布代码于最专业的源码分享网站: Code4App.com
